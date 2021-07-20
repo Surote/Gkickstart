61a5743aa04a9f68869906e7a3841b31ce0f55b7
EC2 Default User <ec2-user@ip-10-0-20-242.ap-southeast-1.compute.internal>
2021-07-20 13:38:54 +0000

    733 C passed

--- codeforce_733/C.py
+++ codeforce_733/C.py
@@ -1,5 +1,4 @@
-# k-k//4
-import math
+
 for testcase in range(int(input())):
     nstages = int(input())
     yourscore = list(map(int,input().split()))
@@ -7,27 +6,36 @@ for testcase in range(int(input())):
     yourscore = sorted(yourscore)
     ilyascore = sorted(ilyascore)
     #print(nstages,yourscore,ilyascore)
-    scoreuse = nstages-(nstages//4)
+    scoreuse = nstages//4
     #print(sum(yourscore[len(yourscore)-scoreuse:]) , sum(ilyascore[len(ilyascore)-scoreuse:]))
-    if sum(yourscore[len(yourscore)-scoreuse:]) > sum(ilyascore[len(ilyascore)-scoreuse:]):
-        print('0')
+    prefixscore1 = sum(yourscore[scoreuse:])
+    prefixscore2 = sum(ilyascore[scoreuse:])
+    if prefixscore1 >= prefixscore2:
+        print(0)
     else:
-        '''
         count=0
-        while sum(yourscore[len(yourscore)-scoreuse:]) < sum(ilyascore[len(ilyascore)-scoreuse:]):
-            
+        p1 = scoreuse
+        p2 = scoreuse-1
+        print(p1,p2)
+        while prefixscore1 < prefixscore2:
             count+=1
-            # increase additional by 1
-            nstages=nstages+1
-            yourscore.append(100)
-            ilyascore.insert(0,0)
-            scoreuse = nstages-(nstages//4)
-        '''
-        maxilya = sum(ilyascore)
-        print(math.ceil((maxilya-sum(yourscore[len(yourscore)-scoreuse:]))/100))
+            nstages+=1
+            #print('BE',nstages,nstages-nstages//4,prefixscore1,prefixscore2)
+            if p2 >= 0 and nstages%4!=0:
+                prefixscore2+=ilyascore[p2]
+                p2-=1
+            if nstages%4==0 and p1<len(yourscore):
+                prefixscore1-=yourscore[p1]
+                p1+=1
+            elif p1<=len(yourscore) and nstages%4==0:
+                prefixscore1-=100
+            prefixscore1+=100
+            #print('AF',nstages,nstages-nstages//4,prefixscore1,prefixscore2)
+            
+        
         #    print(scoreuse,yourscore,ilyascore,nstages,count)
         #    print(sum(yourscore[len(yourscore)-scoreuse:]) , sum(ilyascore[len(ilyascore)-scoreuse:]))
-        #print(count)
+        print(count)
             
 
 
