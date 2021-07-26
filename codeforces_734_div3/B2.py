d7df795bc24c6a7144e035e92dda66ec9f3998e4
EC2 Default User <ec2-user@ip-10-0-20-242.ap-southeast-1.compute.internal>
2021-07-26 10:09:05 +0000

    B2 743 div3 passed

--- codeforces_734_div3/B2.py
+++ codeforces_734_div3/B2.py
@@ -1,56 +1,22 @@
-from collections import Counter
-t=int(input())
-for i in range(t):
-    ans=[]
-    intlistcheck=[]
+for _ in range(int(input())):
     n,k=map(int,input().split())
-    intlist=list(map(int,input().split()))
-    print(Counter(intlist))
-    print(n,k)
-    for j in intlist:
-        intlistcheck.append(j)
-        mincolor=0
-        minlist=[]
-        flag=0
-        for m in range(1,k+1):
-            if len(ans)!=0 and m not in ans:
-                mincolor=m
-                flag=1
-                break
-            else:
-                mincolor=1
-        #all number in ans
-        if flag==0:
-            for l in range(1,k+1):
-                minlist.append(ans.count(l))
-            mincolor=minlist.index(min(minlist))+1
+    num=list(map(int,input().split()))
+    dic={}
+    for i in range(n):
+        if num[i] not in dic:
+            dic[num[i]]=[]
+        dic[num[i]].append(i)
+    #print(dic)
+    ans=[0]*n
+    temp=[]
+    for j in dic:
+        for m in range(min(k,len(dic[j]))):
+            temp.append(dic[j][m])
+            if len(temp) == k:
+               # print(temp)
+                for fil in range(k):
+                    ans[temp[fil]]=fil+1
+                temp=[]
+        #print(temp)
 
-        print(mincolor)
-
-        if intlistcheck.count(j) == 1:
-            ans.append(mincolor)
-        elif intlistcheck.count(j) > k:
-            ans.append(0)
-            continue
-        else:
-            # if in inlistcheck
-
-            usecolor=[]
-            for pp in intlistcheck:
-                if pp == j:
-                    usecolor.append(ans[intlistcheck.index(pp)])
-                    print(ans[intlistcheck.index(pp)])
-            if mincolor not in usecolor:
-                ans.append(mincolor)
-                continue
-            for kk in range(1,k+1):
-                if kk not in usecolor:
-                    ans.append(kk)
-                    break
-            
-            
-        
-        
-    print(intlist)
-    print(ans,intlistcheck)
-    #print((" ").join(ans))
+    print(*ans)
